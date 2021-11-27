b54229f7d79ddc3429f920e3c9f992d39624ba2a
Hari Dandapani <haridandapani@gmail.com>
2021-11-26 22:38:17 -0500

    adding ability to pad with start and end symbols

--- seq2seq_code/openfiles.py
+++ seq2seq_code/openfiles.py
@@ -51,7 +51,7 @@ def addToDict(dicter, char_list):
     
     return seqs
 
-def listToNumpyWindowed(sequences, window_size = 30, padding_symbol = -1):
+def listToNumpyWindowed(sequences, window_size = 30, padding_symbol = -1, add_start = True, start_symbol = 50, add_stop = True, stop_symbol = 99):
     windowed = list()
     masked = list()
     for seq in sequences:
@@ -59,16 +59,40 @@ def listToNumpyWindowed(sequences, window_size = 30, padding_symbol = -1):
         seqlen = len(seq)
         
         if (seqlen < window_size):
+            
             seqqer = [(seq[i] if i < seqlen else padding_symbol) for i in range(0, window_size)]
             masker = [(1 if i < seqlen else 0) for i in range(0, window_size)]
+
+            if (add_stop):
+                seqqer[seqlen] = stop_symbol
+                seqqer.append(padding_symbol)
+                masker.append(0)
+
+            if (add_start):
+                #seqqer = [start_symbol] + [seqqer]
+                seqqer.insert(0, start_symbol)
+                #masker = [0] + [masker]
+                masker.insert(0, 0)
+            
             windowed.append(seqqer)
             masked.append(masker)
             
             #print(seqqer)
             #print(len(seqqer))
         elif (seqlen == window_size):
-            windowed.append(seq)
+            seqqer = seq
             masker = [(1 if i < seqlen else 0) for i in range(0, window_size)]
+
+            if (add_stop):
+                seqqer.append(stop_symbol)
+                masker.append(0)
+
+            if (add_start):
+                seqqer.insert(0, start_symbol)
+                masker.insert(0, 0)
+
+
+            windowed.append(seqqer)
             masked.append(masker)
 
         else:
@@ -79,6 +103,20 @@ def listToNumpyWindowed(sequences, window_size = 30, padding_symbol = -1):
                 if (len(temper) > 0 and len(temper) <= window_size):
                     seqqer = [(temper[i] if i < len(temper) else padding_symbol) for i in range(0, window_size)]
                     masker = [(1 if i < len(temper) else 0) for i in range(0, window_size)]
+
+                    if (add_stop):
+                        if (len(temper) == window_size):
+                            seqqer.append(stop_symbol)
+                            masker.append(0)
+                        else:
+                            seqqer[len(temper)] = stop_symbol
+                            seqqer.append(padding_symbol)
+                            masker.append(0)
+                        
+                    if (add_start):
+                        seqqer.insert(0, start_symbol)
+                        masker.insert(0, 0)
+                    
                     windowed.append(seqqer)
                     masked.append(masker)
 
