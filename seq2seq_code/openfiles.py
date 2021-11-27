f4c37f8d092685b0b615595411b1b93aca9c3e36
Hari Dandapani <haridandapani@gmail.com>
2021-11-26 21:51:40 -0500

    completing preprocessing with masks and windows

--- seq2seq_code/openfiles.py
+++ seq2seq_code/openfiles.py
@@ -5,11 +5,11 @@ def opener(filename : str):
     with open(filename, newline='') as csvfile:
         filereader = csv.reader(csvfile, delimiter=',', quotechar='|')
 
-        seq_language = dict() # 2
+        seq_vocab = dict() # 2
         seq_sequences = list()
-        sst8_language = dict() # 3
+        sst8_vocab = dict() # 3
         sst8_sequences = list()
-        sst3_language = dict() # 4
+        sst3_vocab = dict() # 4
         sst3_sequences = list()
         
         counter = 0
@@ -20,23 +20,69 @@ def opener(filename : str):
                 sst8 = list(row[3])
                 sst3 = list(row[4])
 
-                seq_nums = addToDict(seq_language, seq)
-                sst8_nums = addToDict(sst8_language, sst8)
-                sst3_nums = addToDict(sst3_language, sst3)
+                seq_nums = addToDict(seq_vocab, seq)
+                sst8_nums = addToDict(sst8_vocab, sst8)
+                sst3_nums = addToDict(sst3_vocab, sst3)
+
+                #print(seq_nums)
                 
                 seq_sequences.append(seq_nums)
                 sst8_sequences.append(sst8_nums)
                 sst3_sequences.append(sst3_nums)
     
-        print(len(seq_sequences))
-
-        return seq_language, seq_sequences, sst8_language, sst8_sequences, sst3_language, sst3_sequences
+        #print(len(seq_sequences))
+        #print(seq_sequences[0:5])
+        seq_window, seq_mask = listToNumpyWindowed(seq_sequences)
+        sst8_window, sst8_mask = listToNumpyWindowed(sst8_sequences)
+        sst3_window, sst3_mask = listToNumpyWindowed(sst3_sequences)
+        
+        return seq_vocab, seq_window, seq_mask, sst8_vocab, sst8_window, sst8_mask, sst3_vocab, sst3_window, sst3_mask
             
 def addToDict(dicter, char_list):
     for char in char_list:
         if char not in dicter:
-            value = 0 if len(dicter) == 0 else max(dicter.values())
+            value = 0 if len(dicter) == 0 else max(dicter.values()) + 1
             dicter[char] = value
-    return [dicter[i] for i in char_list]
+            
+    seqs = [dicter[i] for i in char_list]
+    
+    return seqs
+
+def listToNumpyWindowed(sequences, window_size = 30, padding_symbol = -1):
+    windowed = list()
+    masked = list()
+    for seq in sequences:
+        
+        seqlen = len(seq)
+        
+        if (seqlen < window_size):
+            seqqer = [(seq[i] if i < seqlen else padding_symbol) for i in range(0, window_size)]
+            masker = [(1 if i < seqlen else 0) for i in range(0, window_size)]
+            windowed.append(seqqer)
+            masked.append(masker)
+            
+            #print(seqqer)
+            #print(len(seqqer))
+        elif (seqlen == window_size):
+            windowed.append(seq)
+            masker = [(1 if i < seqlen else 0) for i in range(0, window_size)]
+            masked.append(masker)
+
+        else:
+
+            for k in range(0, int(seqlen/window_size) + 1):
+                temper = seq[k * window_size : (k+ 1) * window_size]
+                
+                if (len(temper) > 0 and len(temper) <= window_size):
+                    seqqer = [(temper[i] if i < len(temper) else padding_symbol) for i in range(0, window_size)]
+                    masker = [(1 if i < len(temper) else 0) for i in range(0, window_size)]
+                    windowed.append(seqqer)
+                    masked.append(masker)
+
+    print(len(windowed))
+    print(windowed[len(windowed) - 2])
+    print(windowed[len(windowed) - 1])
+    return windowed, masked
+    
 
 opener("../protein_secondary_structure_data/2018-06-06-pdb-intersect-pisces.csv")
