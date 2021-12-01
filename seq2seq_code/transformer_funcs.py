08287cf486bbff0fdaa8bef68f498cdf2903bfc2
Helen Zhang <helenzhang@helens-mbp.devices.brown.edu>
2021-12-01 18:16:33 -0500

    Small changes

--- seq2seq_code/transformer_funcs.py
+++ seq2seq_code/transformer_funcs.py
@@ -8,8 +8,6 @@ av = AttentionVis()
 @av.att_mat_func
 def Attention_Matrix(K, Q, use_mask=False):
 	"""
-	STUDENT MUST WRITE:
-
 	This functions runs a single attention head.
 
 	:param K: is [batch_size x window_size_keys x embedding_size]
@@ -17,32 +15,16 @@ def Attention_Matrix(K, Q, use_mask=False):
 	:return: attention matrix
 	"""
 	print("ATTENTION MATRIX")
-	window_size_queries = Q.get_shape()[1] # window size of queries
-	window_size_keys = K.get_shape()[1] # window size of keys
-	mask = tf.convert_to_tensor(value=np.transpose(np.tril(np.ones((window_size_queries,window_size_keys))*np.NINF,-1),(1,0)),dtype=tf.float32)
-	atten_mask = tf.tile(tf.reshape(mask,[-1,window_size_queries,window_size_keys]),[tf.shape(input=K)[0],1,1])
-
-	# TODO:
-	# 1) compute attention weights using queries and key matrices (if use_mask==True, then make sure to add the attention mask before softmax)
-	# 2) return the attention matrix
+	window_size_queries = Q.get_shape()[1]
+	window_size_keys = K.get_shape()[1]
+	mask = tf.convert_to_tensor(value=np.transpose(np.tril(np.ones((window_size_queries, window_size_keys))*np.NINF, -1), (1, 0)),dtype=tf.float32)
+	atten_mask = tf.tile(tf.reshape(mask, [-1, window_size_queries, window_size_keys]), [tf.shape(input=K)[0], 1, 1])
 
 	dot = tf.matmul(Q, tf.transpose(K, perm=[0, 2, 1]))
 	scale = dot / 8
 	if use_mask:
 		scale = scale + atten_mask
 	attention = tf.nn.softmax(scale)
-
-
-	# Remember: 
-	# - Q is [batch_size x window_size_queries x embedding_size]
-	# - K is [batch_size x window_size_keys x embedding_size]
-	# - Mask is [batch_size x window_size_queries x window_size_keys]
-
-
-	# Here, queries are matmuled with the transpose of keys to produce for every query vector, weights per key vector. 
-	# This can be thought of as: for every query word, how much should I pay attention to the other words in this window?
-	# Those weights are then used to create linear combinations of the corresponding values for each query.
-	# Those queries will become the new embeddings.
 
 	return attention
 
@@ -53,21 +35,14 @@ class Atten_Head(tf.keras.layers.Layer):
 
 		self.use_mask = use_mask
 
-		# Initialize the weight matrices for K, V, and Q.
-		# They should be able to multiply an input_size vector to produce an output_size vector 
-		# Hint: use self.add_weight(...)
-
 		self.K = self.add_weight(shape=[input_size, output_size])
 		self.V = self.add_weight(shape=[input_size, output_size])
 		self.Q = self.add_weight(shape=[input_size, output_size])
-
 		
 	@tf.function
 	def call(self, inputs_for_keys, inputs_for_values, inputs_for_queries):
 
 		"""
-		STUDENT MUST WRITE:
-
 		This functions runs a single attention head.
 
 		:param inputs_for_keys: tensor of [batch_size x [ENG/FRN]_WINDOW_SIZE x input_size ]
@@ -75,11 +50,6 @@ class Atten_Head(tf.keras.layers.Layer):
 		:param inputs_for_queries: tensor of [batch_size x [ENG/FRN]_WINDOW_SIZE x input_size ]
 		:return: tensor of [BATCH_SIZE x (ENG/FRN)_WINDOW_SIZE x output_size ]
 		"""
-
-		# TODO:
-		# - Apply 3 matrices to turn inputs into keys, values, and queries. You will need to use tf.tensordot for this. 
-		# - Call Attention_Matrix with the keys and queries, and with self.use_mask.
-		# - Apply the attention matrix to the values
 
 		K = tf.tensordot(inputs_for_keys, self.K, [2, 0])
 		V = tf.tensordot(inputs_for_values, self.V, [2, 0])
@@ -90,36 +60,6 @@ class Atten_Head(tf.keras.layers.Layer):
 		V = tf.matmul(attention_matrix, V)
 
 		return V
-
-
-
-class Multi_Headed(tf.keras.layers.Layer):
-	def __init__(self, emb_sz, use_mask):
-		super(Multi_Headed, self).__init__()
-		
-		# TODO:
-		# Initialize heads
-
-	@tf.function
-	def call(self, inputs_for_keys, inputs_for_values, inputs_for_queries):
-		"""
-		FOR CS2470 STUDENTS:
-
-		This functions runs a multiheaded attention layer.
-
-		Requirements:
-			- Splits data for 3 different heads of size embed_sz/3
-			- Create three different attention heads
-			- Concatenate the outputs of these heads together
-			- Apply a linear layer
-
-		:param inputs_for_keys: tensor of [batch_size x [ENG/FRN]_WINDOW_SIZE x input_size ]
-		:param inputs_for_values: tensor of [batch_size x [ENG/FRN]_WINDOW_SIZE x input_size ]
-		:param inputs_for_queries: tensor of [batch_size x [ENG/FRN]_WINDOW_SIZE x input_size ]
-		:return: tensor of [BATCH_SIZE x (ENG/FRN)_WINDOW_SIZE x output_size ]
-		"""
-
-		return None
 
 
 class Feed_Forwards(tf.keras.layers.Layer):
@@ -186,21 +126,22 @@ class Transformer_Block(tf.keras.layers.Layer):
 		"""
 
 		with av.trans_block(self.is_decoder):
-			atten_out = self.self_atten(inputs,inputs,inputs)
-		atten_out+=inputs
+			atten_out = self.self_atten(inputs, inputs, inputs)
+		atten_out += inputs
 		atten_normalized = self.layer_norm(atten_out)
 
 		if self.is_decoder:
-			assert context is not None,"Decoder blocks require context"
-			context_atten_out = self.self_context_atten(context,context,atten_normalized)
-			context_atten_out+=atten_normalized
+			assert context is not None, "Decoder blocks require context"
+			context_atten_out = self.self_context_atten(context, context, atten_normalized)
+			context_atten_out += atten_normalized
 			atten_normalized = self.layer_norm(context_atten_out)
 
 		ff_out=self.ff_layer(atten_normalized)
-		ff_out+=atten_normalized
+		ff_out += atten_normalized
 		ff_norm = self.layer_norm(ff_out)
 
 		return tf.nn.relu(ff_norm)
+
 
 class Position_Encoding_Layer(tf.keras.layers.Layer):
 	def __init__(self, window_sz, emb_sz):
